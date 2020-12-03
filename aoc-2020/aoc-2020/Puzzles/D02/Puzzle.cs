11ef54a3df72300e92889cfc4e21e6d3e4c91404
Kelsey Mott <31663844+kmoth@users.noreply.github.com>
2020-12-03 10:04:04 -0500

    day 2 solved

--- aoc-2020/aoc-2020/Puzzles/D02/Puzzle.cs
+++ aoc-2020/aoc-2020/Puzzles/D02/Puzzle.cs
@@ -6,38 +6,73 @@ namespace AoC.D02 {
 	public class Puzzle : BasePuzzle {
 		
 		public override void Execute() {
-			List<int> convertedInput = Input.Select(int.Parse).ToList();
-			int result = FindPair(convertedInput, 2020);
-			Console.WriteLine($"result: {result}");
+			List<CorruptedPassword> convertedInput = Input.Select(
+				pInput => new CorruptedPassword(pInput)).ToList();
+			
+			int validPasswordCount = convertedInput.Count(
+				ValidatePasswordPart1);
+			
+			Console.WriteLine($"validPasswordCount: {validPasswordCount}");
+			
+			validPasswordCount = convertedInput.Count(
+				ValidatePasswordPart2);
 
-			result = FindTrio(convertedInput, 2020);
-			Console.WriteLine($"result: {result}");
+			Console.WriteLine($"validPasswordCount: {validPasswordCount}");
+		}
+
+		private static bool ValidatePasswordPart1(CorruptedPassword pCorruptedPassword) {
+			int characterCount = pCorruptedPassword.Password.Count(
+				pCharacter => pCharacter == pCorruptedPassword.Character);
+
+			return characterCount >= pCorruptedPassword.Min 
+			       && characterCount <= pCorruptedPassword.Max;
 		}
 		
-		private int FindPair(List<int> pInputs, int pMatchSum) {
-			foreach(int inputA in pInputs) {
-				foreach(int inputB in pInputs) {
-					if(inputA + inputB == pMatchSum) {
-						return inputA * inputB;
-					}
-				}
+		private static bool ValidatePasswordPart2(CorruptedPassword pCorruptedPassword) {
+			char char1 = SafelyGetChar(pCorruptedPassword.Password, pCorruptedPassword.Min);
+			char char2 = SafelyGetChar(pCorruptedPassword.Password, pCorruptedPassword.Max);
+			bool isValid = char1 == pCorruptedPassword.Character ^ char2 == pCorruptedPassword.Character;
+			if(isValid) {
+				
 			}
-
-			return -1;
+			Console.WriteLine($"{isValid} - {pCorruptedPassword}, 1={char1}, 2={char2}");
+			return isValid;
 		}
+
+		private static char SafelyGetChar(string pPassword, int pIndex) {
+			// IT'S NOT ZERO-BASED
+			int adjustedIndex = pIndex - 1;
+			return adjustedIndex < pPassword.Length
+				? pPassword[adjustedIndex]
+				: default;
+		}
+
+	}
+
+	public readonly struct CorruptedPassword {
+
+		private readonly string _raw;
+
+		public string Password { get; }
+
+		public char Character { get; }
+
+		public int Max { get; }
+
+		public int Min { get; }
 		
-		private int FindTrio(List<int> pInputs, int pMatchSum) {
-			foreach(int inputA in pInputs) {
-				foreach(int inputB in pInputs) {
-					foreach(int inputC in pInputs) {
-						if(inputA + inputB + inputC == pMatchSum) {
-							return inputA * inputB * inputC;
-						}
-					}
-				}
-			}
-
-			return -1;
+		public CorruptedPassword(string pInput) {
+			_raw = pInput;
+			string[] bits = _raw.Split(' ');
+			Min = Convert.ToInt32(bits[0].Split('-')[0]);
+			Max = Convert.ToInt32(bits[0].Split('-')[1]);
+			Character = bits[1][0];
+			Password = bits[2];
 		}
+
+		public override string ToString() {
+			return _raw;
+		}
+
 	}
 }
