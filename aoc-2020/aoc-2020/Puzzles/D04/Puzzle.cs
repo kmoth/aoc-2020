b27a74ceff786be3b0bfec5b5c577149ab0dc4bd
Kelsey Mott <31663844+kmoth@users.noreply.github.com>
2020-12-04 12:19:24 -0500

    day foooourrrr

--- aoc-2020/aoc-2020/Puzzles/D04/Puzzle.cs
+++ aoc-2020/aoc-2020/Puzzles/D04/Puzzle.cs
@@ -1,8 +1,179 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace AoC.D04 {
 	public class Puzzle : BasePuzzle {
 		
 		public override void Execute() {
+			List<Passport> passports = new List<Passport>();
+			Passport passport = new Passport();
+			StringBuilder passportData = new StringBuilder();
+			foreach(string line in Input) {
+				if(string.IsNullOrWhiteSpace(line)) {
+					passport.Parse(passportData.ToString());
+					passports.Add(passport);
+					passportData.Clear();
+					passport = new Passport();
+					continue;
+				}
+				
+				passportData.Append($"{line} ");
+			}
 			
+			passport.Parse(passportData.ToString());
+			passports.Add(passport);
+			
+			Console.WriteLine($"Passports found: {passports.Count}");
+
+			int validPassports = ValidatePassports(passports);
+			Console.WriteLine($"valid passports: {validPassports}");
 		}
+
+		private int ValidatePassports(List<Passport> pPassports) {
+			return pPassports.Count(pPassport => pPassport.IsValid);
+		}
+
+	}
+
+	public struct Passport {
+
+		private int _validCreds;
+		private int _requiredCreds;
+
+		public bool IsValid { get; private set; }
+
+		public void Parse(string pRawData) {
+			string[] creds = pRawData.Split(' ');
+			_validCreds = 0;
+			_requiredCreds = 7;
+			foreach(string cred in creds) {
+				ParseCred(cred);
+			}
+
+			IsValid = _validCreds == _requiredCreds;
+		}
+
+		private void ParseCred(string pCred) {
+			string[] credPair = pCred.Split(':');
+			switch(credPair[0]) {
+				case "byr":
+					ValidateByr(credPair[1]);
+					break;
+				case "iyr":
+					ValidateIyr(credPair[1]);
+					break;
+				case "eyr":
+					ValidateEyr(credPair[1]);
+					break;
+				case "hgt":
+					ValidateHgt(credPair[1]);
+					break;
+				case "hcl":
+					ValidateHcl(credPair[1]);
+					break;
+				case "ecl":
+					ValidateEcl(credPair[1]);
+					break;
+				case "pid":
+					ValidatePid(credPair[1]);
+					break;
+			}
+		}
+
+		private void ValidateByr(string pValue) {
+			// (Birth Year) - four digits; at least 1920 and at most 2002.
+			if(!int.TryParse(pValue, out int convertedValue)
+			|| !InRange(convertedValue, 1920, 2002)) {
+				return;
+			}
+			_validCreds++;
+		}
+
+		private void ValidateIyr(string pValue) {
+			// (Issue Year) - four digits; at least 2010 and at most 2020.
+			if(!int.TryParse(pValue, out int convertedValue)
+			   || !InRange(convertedValue, 2010, 2020)) {
+				return;
+			}
+			_validCreds++;
+		}
+		
+		private void ValidateEyr(string pValue) {
+			// (Expiration Year) - four digits; at least 2020 and at most 2030.
+			if(!int.TryParse(pValue, out int convertedValue)
+			   || !InRange(convertedValue, 2020, 2030)) {
+				return;
+			}
+			_validCreds++;
+		}
+		
+		private void ValidateHgt(string pValue) {
+			// hgt (Height) - a number followed by either cm or in:
+			// If cm, the number must be at least 150 and at most 193.
+			// If in, the number must be at least 59 and at most 76.
+			string unit = pValue.Substring(pValue.Length - 2);
+			
+			if(!ParseHeightValue(pValue, unit, out int height)) {
+				return;
+			}
+			
+			switch(unit) {
+				case "cm":
+					if(InRange(height, 150, 193)) {
+						_validCreds++;
+					}
+					break;
+				case "in":
+					if(InRange(height, 59, 76)) {
+						_validCreds++;
+					}
+					break;
+			}
+		}
+
+		private bool ParseHeightValue(string pValue, string pUnit, out int pHeight) {
+			string heightString = pValue.Replace(pUnit, "");
+			return int.TryParse(heightString, out pHeight);
+		}
+
+		private void ValidateHcl(string pValue) {
+			// (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+			Match match = Regex.Match(pValue, @"^#[a-f0-9]{6}$");
+			if(!match.Success) {
+				return;
+			}
+			_validCreds++;
+		}
+		
+		private void ValidateEcl(string pValue) {
+			// (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+			switch(pValue) {
+				case "amb":
+				case "blu":
+				case "brn":
+				case "gry":
+				case "grn":
+				case "hzl":
+				case "oth":
+					_validCreds++;
+					break;
+			}
+		}
+		
+		private void ValidatePid(string pValue) {
+			// (Passport ID) - a nine-digit number, including leading zeroes.
+			if(pValue.Length != 9 || !int.TryParse(pValue, out int id)) {
+				return;
+			}
+			_validCreds++;
+		}
+		
+		private bool InRange(in int pValue, int pMin, int pMax) {
+			return pValue >= pMin && pValue <= pMax;
+		}
+
 	}
 }
