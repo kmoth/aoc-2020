6f1a6d33f22d3700b17f5a46aa08e5fccda3b9f9
Kelsey Mott <31663844+kmoth@users.noreply.github.com>
2020-12-03 23:25:27 -0500

    dear day 3. i hated you.

--- aoc-2020/aoc-2020/Puzzles/D03/Puzzle.cs
+++ aoc-2020/aoc-2020/Puzzles/D03/Puzzle.cs
@@ -4,40 +4,61 @@ using System.Linq;
 
 namespace AoC.D03 {
 	public class Puzzle : BasePuzzle {
-		
+
+		private List<string> _lines;
+		private int _lineLength;
+
+		private readonly Dictionary<char,int> _charValue = new Dictionary<char,int> {
+			{'#',1},
+			{'.',0}
+		};
+
 		public override void Execute() {
-			List<int> convertedInput = Input.Select(int.Parse).ToList();
-			int result = FindPair(convertedInput, 2020);
-			Console.WriteLine($"result: {result}");
-
-			result = FindTrio(convertedInput, 2020);
-			Console.WriteLine($"result: {result}");
+			_lines = Input.ToList();
+			_lineLength = _lines.First().Length;
+			
+			PartOne();
+			PartTwo();
 		}
-		
-		private int FindPair(List<int> pInputs, int pMatchSum) {
-			foreach(int inputA in pInputs) {
-				foreach(int inputB in pInputs) {
-					if(inputA + inputB == pMatchSum) {
-						return inputA * inputB;
-					}
+
+		private void PartOne() {
+			int treesFound = GetTreesAlongSlope(3, 1);
+			Console.WriteLine($"tree count: {treesFound}");
+		}
+
+		private void PartTwo() {
+			long treesFound1 = GetTreesAlongSlope(1, 1);
+			long treesFound2 = GetTreesAlongSlope(3, 1);
+			long treesFound3 = GetTreesAlongSlope(5, 1);
+			long treesFound4 = GetTreesAlongSlope(7, 1);
+			long treesFound5 = GetTreesAlongSlope(1, 2);
+			
+			Console.WriteLine($"tree count 1: {treesFound1}");
+			Console.WriteLine($"tree count 2: {treesFound2}");
+			Console.WriteLine($"tree count 3: {treesFound3}");
+			Console.WriteLine($"tree count 4: {treesFound4}");
+			Console.WriteLine($"tree count 5: {treesFound5}");
+			
+			// ARGHFG!!1! INTS ARE NOT ENOUGH
+			long product = treesFound1 * treesFound2 * treesFound3 * treesFound4 * treesFound5;
+			
+			Console.WriteLine($"product: {product}");
+		}
+
+		private int GetTreesAlongSlope(int pX, int pY) {
+			int lastX = pX;
+			int treeCount = 0;
+			for(int y = pY; y < _lines.Count; y += pY) {
+				treeCount += _charValue[_lines[y][lastX]];
+				lastX += pX;
+				if(lastX >= _lineLength) {
+					lastX -= _lineLength;
 				}
 			}
-
-			return -1;
+			return treeCount;
 		}
-		
-		private int FindTrio(List<int> pInputs, int pMatchSum) {
-			foreach(int inputA in pInputs) {
-				foreach(int inputB in pInputs) {
-					foreach(int inputC in pInputs) {
-						if(inputA + inputB + inputC == pMatchSum) {
-							return inputA * inputB * inputC;
-						}
-					}
-				}
-			}
 
-			return -1;
-		}
+
 	}
+
 }
