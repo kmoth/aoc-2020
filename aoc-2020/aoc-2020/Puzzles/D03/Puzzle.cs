6626972f274303dcc8ef72a50c7d7b4ffdd30cfe
Kelsey Mott <31663844+kmoth@users.noreply.github.com>
2020-12-03 23:53:15 -0500

    cleanup the future puzzles in case i actually do them

--- aoc-2020/aoc-2020/Puzzles/D03/Puzzle.cs
+++ aoc-2020/aoc-2020/Puzzles/D03/Puzzle.cs
@@ -33,16 +33,10 @@ namespace AoC.D03 {
 			long treesFound4 = GetTreesAlongSlope(7, 1);
 			long treesFound5 = GetTreesAlongSlope(1, 2);
 			
-			Console.WriteLine($"tree count 1: {treesFound1}");
-			Console.WriteLine($"tree count 2: {treesFound2}");
-			Console.WriteLine($"tree count 3: {treesFound3}");
-			Console.WriteLine($"tree count 4: {treesFound4}");
-			Console.WriteLine($"tree count 5: {treesFound5}");
-			
 			// ARGHFG!!1! INTS ARE NOT ENOUGH
 			long product = treesFound1 * treesFound2 * treesFound3 * treesFound4 * treesFound5;
 			
-			Console.WriteLine($"product: {product}");
+			Console.WriteLine($"tree product: {product}");
 		}
 
 		private uint GetTreesAlongSlope(int pX, int pY) {
