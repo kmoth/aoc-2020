4489e5780739fffd077fd6fc9c4feda9280b1328
Kelsey Mott <kelsey@relis.sh>
2021-01-04 20:43:18 -0500

    progress on day 9

--- aoc-2020/aoc-2020/Puzzles/D09/Puzzle.cs
+++ aoc-2020/aoc-2020/Puzzles/D09/Puzzle.cs
@@ -1,17 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AoC.D09 {
 	public class Puzzle : BasePuzzle {
-		
+
+		private readonly List<int> _convertedInput;
+
 		public Puzzle() {
-			// _convertedInput = LoadInputLines().ToList();
+			_convertedInput = LoadInputLines(true)
+				.Select(int.Parse)
+				.ToList();
 		}
 		
 		public override string SolvePartOne() {
-			return "INCOMPLETE";
+			XmasCracker xmasCracker = new XmasCracker(5, _convertedInput);
+			return xmasCracker.FirstInvalid();
 		}
 		
 		public override string SolvePartTwo() {
 			return "INCOMPLETE";
 		}
 		
+	}
+
+	public class XmasCracker {
+
+		private int _preambleSize;
+		private List<int> _input;
+
+		public XmasCracker(int pPreambleSize, List<int> pInput) {
+			_preambleSize = pPreambleSize;
+			_input = pInput;
+		}
+
+		public string FirstInvalid() {
+			return "INCOMPLETE";
+		}
+
 	}
 }
